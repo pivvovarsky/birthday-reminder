0caa70d12e4c0827f294936ead7557ad66c4ddcd
Szczepan Piwowarski <pivvovarski@gmail.com>
2022-05-05 00:48:12 +0200

    Adding the whole project

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+      id: 1,
+      name: 'Szczepan Piwowarski',
+      age: 21,
+      image:
+        'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
+    },
+    {
+        id: 2,
+        name: 'Mateusz Pol',
+        age: 23,
+        image:
+          'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
+      },
+      {
+        id: 3,
+        name: 'Anna Kowalska',
+        age: 18,
+        image:
+          'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-6.jpg',
+      },
+      {
+        id: 4,
+        name: 'Andrzej Kowalski',
+        age: 40,
+        image:
+          'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
+      },
+      {
+        id: 5,
+        name: 'Marta Pol',
+        age: 33,
+        image:
+          'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-6.jpg',
+      } 
+];
+
+export default data;
